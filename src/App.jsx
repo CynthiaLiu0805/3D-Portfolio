b13e069023148228f071cd39b5855b886f1e548a
unknown <cynthialiu0805@gmail.com>
2025-05-07 15:36:52 -0400

    about me page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from "./sections/Navbar.jsx";
 import Hero from "./sections/Hero.jsx";
+import About from "./sections/About.jsx";
 
 const App = () => {
     return (
@@ -8,6 +9,7 @@ const App = () => {
             <h1 className="text-2xl text-white underline">hello hello</h1>
             <Navbar />
             <Hero />
+            <About />
 
         </main>
     )
