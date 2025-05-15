07b35b844f9fd91a2f6dca405286485e0179b9f4
unknown <cynthialiu0805@gmail.com>
2025-05-15 17:41:48 -0400

    add texture

--- src/constants/index.js
+++ src/constants/index.js
@@ -29,13 +29,7 @@ export const myProjects = [
         subdesc:
             'Built as a web application using Vue.js, HTML, and CSS, BCEP delivers visualizations of chloride exposure predictions through an intuitive user interface. Python was used to construct the backend database and generate prediction trends based on climate and traffic datasets. The project incorporates Continuous Integration via GitHub Actions for streamlined testing, while stakeholder feedback is managed through GitHub Issues. A tailored requirement validation process ensures ongoing alignment with user needs and supports long-term maintainability.',
         href: 'https://bcep.onrender.com/',
-        texture: '/textures/project/project1.mp4',
-        logo: '/assets/project-logo1.png',
-        logoStyle: {
-            backgroundColor: '#2A1816',
-            border: '0.2px solid #36201D',
-            boxShadow: '0px 0px 60px 0px #AA3C304D',
-        },
+        texture: '/textures/project/waterra.mp4',
         spotlight: '/assets/spotlight1.png',
         tags: [
             {
@@ -67,12 +61,6 @@ export const myProjects = [
             'With LiveDoc, users can experience the future of collaboration, where multiple contributors work together in real time without any lag, by using Next.js and Liveblocks newest features.',
         href: 'https://www.youtube.com/watch?v=y5vE8y_f_OM',
         texture: '/textures/project/project2.mp4',
-        logo: '/assets/project-logo2.png',
-        logoStyle: {
-            backgroundColor: '#13202F',
-            border: '0.2px solid #17293E',
-            boxShadow: '0px 0px 60px 0px #2F6DB54D',
-        },
         spotlight: '/assets/spotlight2.png',
         tags: [
             {
@@ -103,15 +91,7 @@ export const myProjects = [
         subdesc:
             'The platform was built using React and TypeScript, with a custom UI component library designed to present sensor data on a color-coded Leaflet map for geographic and temporal analysis. Backend APIs were developed in Go to query sensor data stored in InfluxDB, a time-series database linked to remote monitoring hardware.',
         href: 'https://github.com/CynthiaLiu0805/Waterra/tree/master',
-        texture: '/textures/project/project3.mp4',
-        logo: '/assets/project-logo3.png',
-        logoStyle: {
-            backgroundColor: '#60f5a1',
-            background:
-                'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
-            border: '0.2px solid rgba(208, 213, 221, 1)',
-            boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
-        },
+        texture: '/textures/project/waterra.mp4',
         spotlight: '/assets/spotlight3.png',
         tags: [
             {
@@ -137,18 +117,43 @@ export const myProjects = [
         ],
     },
     {
-        title: 'Horizon - Online Banking Platform',
-        desc: 'Horizon is a comprehensive online banking platform that offers users a centralized finance management dashboard. It allows users to connect multiple bank accounts, monitor real-time transactions, and seamlessly transfer money to other users.',
+        title: 'Love, Cat & Robot - A Narrative-Driven Pixel Adventure Game',
+        desc: 'Love, Cat & Robot is an RPG game that combines emotional storytelling with retro aesthetics. Players step into Tyra\'s life, navigating her daily routine and uncovering the hidden truths of a mysterious world. Through interaction with two central NPCs—Tyra’s pet cat and Derek, an AI assistant, players are allowed to shape the direction of the story, unlocking different endings based on their decisions. As the plot evolves, the game transforms from a peaceful routine into a haunting experience filled with metafictional twists and darker undertones, keeping players immersed and eager to discover the true endings. ',
         subdesc:
-            'Built with Next.js 14 Appwrite, Dwolla and Plaid, Horizon ensures a smooth and secure banking experience, tailored to meet the needs of modern consumers.',
-        href: 'https://www.youtube.com/watch?v=PuOVqP_cjkE',
-        texture: '/textures/project/project4.mp4',
-        logo: '/assets/project-logo4.png',
-        logoStyle: {
-            backgroundColor: '#0E1F38',
-            border: '0.2px solid #0E2D58',
-            boxShadow: '0px 0px 60px 0px #2F67B64D',
-        },
+            'Developed in Unity, Tyra features pixel art backgrounds to evoke a nostalgic yet atmospheric setting. The gameplay blends exploration, branching dialogues, and mini bullet-hell combat. Players use WASD keys for movement and the mouse for interactions, seamlessly shifting between contestant-based dialogue choices and avatar-based combat sequences. This hybrid interaction model ensures a varied and engaging experience, balancing narrative depth with gameplay challenge. ',
+        href: 'https://github.com/CynthiaLiu0805/Waterra/tree/master',
+        texture: '/textures/project/lovecatrobot.mp4',
+        spotlight: '/assets/spotlight3.png',
+        tags: [
+            {
+                id: 1,
+                name: 'React.js',
+                path: '/assets/react.svg',
+            },
+            {
+                id: 2,
+                name: 'TailwindCSS',
+                path: 'assets/tailwindcss.png',
+            },
+            {
+                id: 3,
+                name: 'TypeScript',
+                path: '/assets/typescript.png',
+            },
+            {
+                id: 4,
+                name: 'Framer Motion',
+                path: '/assets/framer.png',
+            },
+        ],
+    },
+    {
+        title: 'UniCost - Online Banking Platform',
+        desc: 'UniCost aims to peak students\' interest in their financial futures, to give them perspective on the price of school, to break the general trend, and to help them make smart financial decisions.',
+        subdesc:
+            'Built with Node.js and Express on the backend and EJS for frontend templating, UniCost features both guest and registered user modes. MongoDB Atlas serves as the primary database, with bcrypt integrated for secure authentication. Users can input data such as tuition, program length, expected income, and financial aid to receive personalized projections and visualizations generated using Plotly. ',
+        href: 'https://github.com/CynthiaLiu0805/UniCost',
+        texture: '/textures/project/unicost.mp4',
         spotlight: '/assets/spotlight4.png',
         tags: [
             {
@@ -180,12 +185,6 @@ export const myProjects = [
             'Built with Next.js 14, Cloudinary AI, Clerk, and Stripe, Imaginify combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.',
         href: 'https://www.youtube.com/watch?v=Ahwoks_dawU',
         texture: '/textures/project/project5.mp4',
-        logo: '/assets/project-logo5.png',
-        logoStyle: {
-            backgroundColor: '#1C1A43',
-            border: '0.2px solid #252262',
-            boxShadow: '0px 0px 60px 0px #635BFF4D',
-        },
         spotlight: '/assets/spotlight5.png',
         tags: [
             {
